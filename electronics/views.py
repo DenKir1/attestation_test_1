c76f84d5b830a00bae83d242fb5b63ca102e2167
DenKir1 <142805921+DenKir1@users.noreply.github.com>
2024-05-06 15:56:24 +0300

    Update views.py + filter

--- electronics/views.py
+++ electronics/views.py
@@ -6,6 +6,40 @@ from electronics.permissions import IsActive
 from electronics.serializers import SellerSerializer, SellerUpdateSerializer
 
 
+class ProductViewSet(viewsets.ModelViewSet):
+    """
+    Контроллер для модели Продуктов
+    """
+
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def get_permissions(self):
+        permission_classes = [AllowAny, ]
+        if self.action in ('retrieve', 'list', 'create'):
+            permission_classes = [IsAuthenticated, ]
+        if self.action in ('update', 'destroy', 'partial_update'):
+            permission_classes = [IsAdmin, ]
+        return [permission() for permission in permission_classes]
+
+
+class ContactViewSet(viewsets.ModelViewSet):
+    """
+    Контроллер для модели Контактов
+    """
+
+    queryset = Contact.objects.all()
+    serializer_class = ProductSerializer
+
+    def get_permissions(self):
+        permission_classes = [AllowAny, ]
+        if self.action in ('retrieve', 'list', 'create'):
+            permission_classes = [IsAuthenticated, ]
+        if self.action in ('update', 'destroy', 'partial_update'):
+            permission_classes = [IsAdmin, ]
+        return [permission() for permission in permission_classes]
+
+
 class SellerViewSet(ModelViewSet):
     # queryset = Seller.objects.all()
     # serializer_class = SellerSerializer
@@ -36,3 +70,12 @@ class SellerViewSet(ModelViewSet):
         if self.action in ('update', 'destroy', 'partial_update'):
             permission_classes = [IsAuthenticated, IsOwner | IsAdmin, ]
         return [permission() for permission in permission_classes]
+
+
+# filter to file
+import django_filters
+class SellerFilter(django_filters.FilterSet):
+    contact__country = django_filters.CharFilter(field_name='country', lookup_expr='icontains')
+    class Meta:
+        model = Seller
+        fields = ['contact__country']
