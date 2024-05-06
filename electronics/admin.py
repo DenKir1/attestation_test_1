e7efb54d704b5d133a6f00eb1f94f88f54f661e9
DenKir1 <142805921+DenKir1@users.noreply.github.com>
2024-05-06 15:23:44 +0300

    Update admin.py

--- electronics/admin.py
+++ electronics/admin.py
@@ -10,29 +10,16 @@ admin.site.register(Contact)
 def debt_nulled(modeladmin, request, queryset):
     queryset.update(debt=0)
 
-# def linkify(field_name):
-#     def _linkify(obj):
-#         linked_obj = getattr(obj, field_name)
-#         if linked_obj is None:
-#             return '-'
-#         app_label = linked_obj._meta.app_label
-#         model_name = linked_obj._meta.model_name
-#         view_name = f'admin:{app_label}_{model_name}_change'
-#         link_url = reverse(view_name, args=[linked_obj.pk])
-#         return format_html('<a href="{}">{}</a>', link_url, linked_obj)
-#     _linkify.short_description = field_name
-#     return _linkify
-
 
 @admin.register(Seller)
 class SellerAdmin(admin.ModelAdmin):
-    list_display = ('name', 'seller_type', 'supplier',)
-    list_display_links = ('name', linkify(field_name='supplier'),)
+    def link_to_suplier(self, obj):
+        link = urlresolvers.reverse('admin:app_bar_change', args=[obj.suplier.id])
+        linkify = format_html('<a href="{}">{}</a>', link, obj.suplier.id) if obj.suplier.id else None
+        linkify.short_description = obj.suplier.name
+        return linkify
+        
+    list_display = ('name', 'seller_type', 'link_to_suplier',)
+
     list_filter = ('seller_type', 'contact__city',)
     actions = [debt_nulled, ]
-
-    # def contact_city(self, obj):
-    #     return obj.contacts.city
-    # def link_to_bar(self, obj):
-    #     link = urlresolvers.reverse('admin:app_bar_change', args=[obj.bar_id])
-    #     return format_html('<a href="{}">{}</a>', link, obj.bar) if obj.bar else None
